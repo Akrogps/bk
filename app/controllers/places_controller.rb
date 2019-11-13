263925b955ecceceebb8aa50ae7f471ea90d8eed
Adrien Blondel <adrien15b@gmail.com>
2019-11-13 16:15:02 +0100

    search category and tag are working

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,8 +3,17 @@ class PlacesController < ApplicationController
   before_action :set_place, only: :show
 
   def index
-    if params[:query] && params[:query][:tags].present?
-      @places_list = Tag.where(name: params[:query][:tags]).map { |tag| tag.places }.flatten.uniq
+    if check_if_filter
+      @places_list = []
+      @places_list_tag = filter_by(Tag, "tags")
+      @places_list_category = filter_by(Category, "categories")
+      if !@places_list_tag.empty? && !@places_list_category.empty?
+        @places_list = @places_list_category & @places_list_tag
+        @places_list.flatten!
+      else
+        @places_list_tag.map { |place| @places_list << place }
+        @places_list_category.map { |place| @places_list << place }
+      end
     else
       @places_list = Place.all
     end
@@ -24,6 +33,18 @@ class PlacesController < ApplicationController
   end
 
   private
+
+  def check_if_filter
+    params[:query] && params[:query][:tags].present? && params[:query][:categories].present?
+  end
+
+  def filter_by(class_title, plural_name)
+    class_title.where(name: query_params.to_h[plural_name].to_a.map { |element| element[0] if element[1] == "1" }.compact).map { |element| element.places }.flatten.uniq
+  end
+
+  def query_params
+    params.require(:query).permit(tags: {}, categories: {})
+  end
 
   def set_place
     @place = Place.find(params[:id])
