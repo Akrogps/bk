91292f785ac1eca3c05c791d0198ec5aea792dee
Baudouin Renie <baudouin@renie.org>
2019-11-16 23:10:45 +0100

    First dropdown with checkboxes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,8 +3,6 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 
 import { initMapbox } from '../plugins/init_mapbox';
 import { initAutocomplete } from '../plugins/init_autocomplete';
-import { showCheckboxes } from '../plugins/init_checkboxes_dropdown';
 
 initMapbox();
 initAutocomplete();
-showCheckboxes();
