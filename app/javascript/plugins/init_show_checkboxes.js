a6a01d3799360230e4598f9f8f5458b2ffb00dd9
Baudouin Renie <baudouin@renie.org>
2019-11-18 11:30:16 +0100

    Change variable name

--- app/javascript/plugins/init_show_checkboxes.js
+++ app/javascript/plugins/init_show_checkboxes.js
@@ -1,22 +1,22 @@
 const initShowCheckboxes = (dropdown_name, select_name) => {
-  let checkboxes = document.getElementById(dropdown_name);
+  let dropdown = document.getElementById(dropdown_name);
   let select = document.getElementById(select_name);
 
   select.addEventListener("click", function(event) {
     if (dropdown_name === "checkboxes1") {
       if (!expanded1) {
-        checkboxes.style.display = "block";
+        dropdown.style.display = "block";
         expanded1 = true;
       } else {
-        checkboxes.style.display = "none";
+        dropdown.style.display = "none";
         expanded1 = false;
       }
     } else {
       if (!expanded2) {
-        checkboxes.style.display = "block";
+        dropdown.style.display = "block";
         expanded2 = true;
       } else {
-        checkboxes.style.display = "none";
+        dropdown.style.display = "none";
         expanded2 = false;
       }
     }
@@ -24,9 +24,9 @@ const initShowCheckboxes = (dropdown_name, select_name) => {
 
   // Click outside feature
   document.addEventListener("click", function(event) {
-    let isClickInside = checkboxes.contains(event.target) || select.contains(event.target);
+    let isClickInside = dropdown.contains(event.target) || select.contains(event.target);
     if (!isClickInside) {
-      checkboxes.style.display = "none";
+      dropdown.style.display = "none";
       if (dropdown_name === "checkboxes1") {
         expanded1 = false;
       } else {
