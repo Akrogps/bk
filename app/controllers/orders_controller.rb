e6d76651e5bdb64d85cf5fdff423411085d85b17
Adrien Blondel <adrien15b@gmail.com>
2019-11-18 19:34:22 +0100

    day 1

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,17 +5,23 @@ class OrdersController < ApplicationController
     @order = Order.find_by(id: session[:order_id])
     @list_items = []
     @list_items = @order.order_lines.map do |line|
-      { product_sku: line.product_sku, price: line.price_cents, amount_of_products: 1 }
+      { name: Book.where(id: line.productable_id)[0].title,
+        images: [Book.where(id: line.productable_id)[0].images.first.photo_url],
+        amount: 60,
+        currency: 'eur',
+        quantity: 2
+      }
     end
     @list_items.flatten!
 
     stripe_session = Stripe::Checkout::Session.create(
       payment_method_types: ['card'],
-      line_items: @list_items,
+      line_items: [@list_items.first],
       success_url: order_url(@order),
       cancel_url: order_url(@order)
     )
+    raise
     @order.update(checkout_session_id: stripe_session.id)
-    redirect_to new_order_payment_path(order)
+    redirect_to new_order_payment_path(@order)
   end
 end
