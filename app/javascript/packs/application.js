ff3bfd6e0fe4c6af15df9b7e17516b2aeccaa198
Felipe del Marmol <felipedelmarmol@gmail.com>
2019-11-18 19:15:44 +0100

    hide map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,8 +4,10 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
 import { initAutocomplete } from '../plugins/init_autocomplete';
 import { initShowCheckboxes } from '../plugins/init_show_checkboxes';
+import { initHideMap } from '../plugins/init_hide_map';
 
 initMapbox();
 initAutocomplete();
 initShowCheckboxes("checkboxes1", "select-box1");
 initShowCheckboxes("checkboxes2", "select-box2");
+initHideMap();
