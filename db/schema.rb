1d98b7aefe7ac6ca032f1683da69365aeafa8515
Adrien Blondel <adrien15b@gmail.com>
2019-11-19 10:56:57 +0100

    add column to order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_18_132807) do
+ActiveRecord::Schema.define(version: 2019_11_19_095452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,6 +97,7 @@ ActiveRecord::Schema.define(version: 2019_11_18_132807) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "price_cents", default: 0, null: false
+    t.string "checkout_stripe_session_id"
   end
 
   create_table "places", force: :cascade do |t|
